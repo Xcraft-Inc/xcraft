396985b244e546ccd6f28bc40a263d9fbba444c6
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-21 14:46:41 +0200

    Cosmetics: reindent.

--- scripts/stage1.js
+++ scripts/stage1.js
@@ -1,25 +1,30 @@
 #!/usr/bin/env node
 
-var sys = require('sys');
-var exec = require('child_process').exec;
+var sys  = require ('sys');
+var exec = require ('child_process').exec;
 
-try {
-  process.chdir(__dirname + '/..');
-  console.log('[stage1] go to the toolchain directory: ' + process.cwd());
+try
+{
+  process.chdir (__dirname + '/..');
+  console.log ('[stage1] go to the toolchain directory: ' + process.cwd ());
 }
-catch (err) {
-  console.log('[stage1]: ' + err);
-}
-
-function puts(error, stdout, stderr) {
-  sys.puts(stdout)
+catch (err)
+{
+  console.log ('[stage1]: ' + err);
 }
 
-console.log('[stage1] install commander');
-try {
-  exec("npm install commander", puts);
-  console.log('[stage1] end of stage one');
+function puts(error, stdout, stderr)
+{
+  sys.puts (stdout)
 }
-catch (err) {
-  console.log('[stage1]: ' + err);
+
+console.log ('[stage1] install commander');
+try
+{
+  exec ("npm install commander", puts);
+  console.log ('[stage1] end of stage one');
+}
+catch (err)
+{
+  console.log ('[stage1]: ' + err);
 }
