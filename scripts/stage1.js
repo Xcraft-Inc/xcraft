1818ea9c0a823c98f7ec5a92e9c9daf5df1fda48
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-21 14:15:36 +0200

    Install commander in the right directory $toolchain/node_modules/* and add error handling.

--- scripts/stage1.js
+++ scripts/stage1.js
@@ -3,5 +3,23 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
 
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("npm install commander", puts);
+try {
+  process.chdir(__dirname + '/..');
+  console.log('[stage1] go to the toolchain directory: ' + process.cwd());
+}
+catch (err) {
+  console.log('[stage1]: ' + err);
+}
+
+function puts(error, stdout, stderr) {
+  sys.puts(stdout)
+}
+
+console.log('[stage1] install commander');
+try {
+  exec("npm install commander", puts);
+  console.log('[stage1] end of stage one');
+}
+catch (err) {
+  console.log('[stage1]: ' + err);
+}
