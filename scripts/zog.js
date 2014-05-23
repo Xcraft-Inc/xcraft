246d2ddae97eb3292a626492f5c3f6a75bd28d41
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-23 16:17:49 +0200

    Add a create package stub to the zog command.

--- scripts/zog.js
+++ scripts/zog.js
@@ -2,9 +2,11 @@
 
 var program = require ('commander');
 var zogWpkg = require ('./zogWpkg.js');
+var zogManager = require ('./zogManager.js');
 
 process.chdir (__dirname + '/..');
 program
   .version ('0.0.1')
   .option ('-w, --wpkg [action]', 'manage the wpkg installation', zogWpkg.action)
+  .option ('create [package]', 'create a new empty package', zogManager.create)
   .parse (process.argv);
