74fbb1a2a3673c450b9d3dc26141c21ec000334f
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-23 09:44:32 +0200

    Cosmetic.

--- scripts/zog.js
+++ scripts/zog.js
@@ -8,5 +8,5 @@ var wpkg = new zogWpkg.wpkgManager ('wpkg');
 process.chdir (__dirname + '/..');
 program
   .version ('0.0.1')
-  .option ('-w, --wpkg [action]', 'Manage the wpkg installation', wpkg.action)
+  .option ('-w, --wpkg [action]', 'manage the wpkg installation', wpkg.action)
   .parse (process.argv);
