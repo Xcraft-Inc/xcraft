016bd83ec090bba202f5c69c8c9c7f867a7949ce
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-26 09:05:04 +0200

    Use the path module where appropriate.

--- scripts/zog.js
+++ scripts/zog.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 var program = require ('commander');
-var zogWpkg = require ('./zogWpkg.js');
+var path    = require ('path');
+
+var zogWpkg    = require ('./zogWpkg.js');
 var zogLokthar = require ('./zogLokthar.js');
 var zogManager = require ('./zogManager.js');
 
-process.chdir (__dirname + '/..');
+process.chdir (path.join (__dirname, '/..'));
 program
   .version ('0.0.1')
   .option ('-w, --wpkg [action]', 'manage the wpkg installation', zogWpkg.action)
