c6d2dfbf66a728120ccdcfb6d412d053629ae50b
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-26 08:36:34 +0200

    Cosmetics: fix coding style.

--- scripts/zogLokthar.js
+++ scripts/zogLokthar.js
@@ -1,61 +1,59 @@
 
+var sys         = require ('sys');
+var path        = require ('path');
+var exec        = require ('child_process').exec;
 var zogPlatform = require ('./lib/zogPlatform');
-var sys  = require ('sys');
-var path  = require ('path');
-var exec = require ('child_process').exec;
+
 var package = 'lokthar';
-var buildDir = path.join(__dirname,"../lokthar/build/");
-var atomDir = path.join(__dirname,"../lokthar/build/atom-shell/");
-var loktharAppDir = path.join(__dirname,"../lokthar/lokthar-app");
+var buildDir = path.join (__dirname, "../lokthar/build/");
+var atomDir = path.join (__dirname, "../lokthar/build/atom-shell/");
+var loktharAppDir = path.join (__dirname, "../lokthar/lokthar-app");
 var cmd = {};
 
 
 var build = function ()
 {
-  exec ('npm install --prefix ' + buildDir + ' ' + buildDir, function (error, stdout, stderr) {
+  exec ('npm install --prefix ' + buildDir + ' ' + buildDir, function (error, stdout, stderr)
+  {
     sys.puts (stdout);
-      if(error === null)
-      {
-        grunt();
-      }
-    });
+    if (error === null)
+      grunt ();
+  });
 }
 
 var grunt = function ()
 {
-  exec ('grunt --gruntfile '+ buildDir + 'gruntfile.js' + ' download-atom-shell', function (error, stdout, stderr) {
+  exec ('grunt --gruntfile '+ buildDir + 'gruntfile.js' + ' download-atom-shell', function (error, stdout, stderr)
+  {
     sys.puts (stdout);
-      if(error === null)
-      {
-        
-      }
-    });
+    if (error === null)
+    {
+      
+    }
+  });
 }
 
 cmd.run = function ()
 {
-  exec (atomDir + 'atom.exe ' + loktharAppDir, function (error, stdout, stderr) {
+  exec (atomDir + 'atom.exe ' + loktharAppDir, function (error, stdout, stderr)
+  {
     sys.puts (stdout);
-      if(error === null)
-      {
-        
-      }
-    });
+    if (error === null)
+    {
+      
+    }
+  });
 }
 
-/**
- * \brief Install the package in /tools.
- */
 cmd.install = function ()
 {
   try
   {
-    exec ('npm install grunt-cli', function (error, stdout, stderr) {
+    exec ('npm install grunt-cli', function (error, stdout, stderr)
+    {
       sys.puts (stdout);
-      if(error === null)
-      {
-        build();
-      }
+      if (error === null)
+        build ();
     });
   }
   catch (err)
@@ -64,17 +62,11 @@ cmd.install = function ()
   }
 }
 
-/**
- * \brief Uninstall the package from /tools.
- */
 cmd.uninstall = function ()
 {
   
 }
 
-/**
- * \brief Actions called from commander with --wpkg.
- */
 exports.action = function (act)
 {
   console.log ('[' + package + '] ' + act);
