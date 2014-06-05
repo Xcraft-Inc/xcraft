2fe584475824bb2d5991ca622a389e4f33f180d2
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-05 17:29:47 +0200

    Cosmetic: move the verbosity argument at the top.

--- scripts/zog.js
+++ scripts/zog.js
@@ -10,8 +10,8 @@ var zogLog     = require ('./lib/zogLog.js')('zog');
 
 program
   .version ('0.0.1')
+  .option ('-v, --verbosity <level>', 'change the verbosity level [0..3] (default: 0)', zogLog.verbosity)
   .option ('-w, --wpkg <action>', 'manage the wpkg installation', zogWpkg.action)
   .option ('-l, --lokthar <action>', 'manage the lokthar installation', zogLokthar.action)
-  .option ('-v, --verbosity <level>', 'change the verbosity level [0..3] (default: 0)', zogLog.verbosity)
   .option ('create <package>', 'create a new empty package', zogManager.create)
   .parse (process.argv);
