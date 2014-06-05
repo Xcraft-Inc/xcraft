8af0fe855323618c2b7f0a2cb06f358f49efa62b
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-05 16:09:41 +0200

    The arguments with commander are mandatory.

--- scripts/zog.js
+++ scripts/zog.js
@@ -10,7 +10,7 @@ var zogManager = require ('./zogManager.js');
 process.chdir (path.join (__dirname, '/..'));
 program
   .version ('0.0.1')
-  .option ('-w, --wpkg [action]', 'manage the wpkg installation', zogWpkg.action)
-  .option ('-l, --lokthar [action]', 'manage the lokthar installation', zogLokthar.action)
-  .option ('create [package]', 'create a new empty package', zogManager.create)
+  .option ('-w, --wpkg <action>', 'manage the wpkg installation', zogWpkg.action)
+  .option ('-l, --lokthar <action>', 'manage the lokthar installation', zogLokthar.action)
+  .option ('create <package>', 'create a new empty package', zogManager.create)
   .parse (process.argv);
