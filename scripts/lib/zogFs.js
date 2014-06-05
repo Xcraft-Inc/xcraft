914231735a23eb438d2938ba69c00f703613e871
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-05 13:23:21 +0200

    Add a filesystem function in order to retrieve a list of directory without the files.

--- scripts/lib/zogFs.js
+++ scripts/lib/zogFs.js
@@ -1,8 +1,10 @@
 
-exports.mkdir = function (path, root)
+var fs   = require ('fs');
+var path = require ('path');
+
+exports.mkdir = function (location, root)
 {
-  var fs   = require ('fs');
-  var dirs = path.split ('/');
+  var dirs = location.split ('/');
   var dir  = dirs.shift ();
   var root = (root || '') + dir + '/';
 
@@ -17,4 +19,20 @@ exports.mkdir = function (path, root)
   }
 
   return !dirs.length || this.mkdir (dirs.join ('/'), root);
+}
+
+exports.lsdir = function (location)
+{
+  var listIn = fs.readdirSync (location);
+  var listOut = [];
+
+  for (var item in listIn)
+  {
+    var dir = path.join (location, listIn[item]);
+    var st = fs.statSync (dir);
+    if (st.isDirectory (dir))
+      listOut.push (listIn[item]);
+  }
+
+  return listOut;
 }
