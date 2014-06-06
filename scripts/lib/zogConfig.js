e34b320841b9809042bc7a6375a8e145c2c51de7
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-06 16:29:49 +0200

    Cosmetic: the indentation uses spaces, not tabs.

--- scripts/lib/zogConfig.js
+++ scripts/lib/zogConfig.js
@@ -2,9 +2,8 @@
 var path = require ('path');
 
 process.chdir (path.join (__dirname, '/../..'));
-exports.libRoot 		= path.resolve ('./scripts/lib/');
+exports.libRoot         = path.resolve ('./scripts/lib/');
 exports.pkgBaseRoot     = path.resolve ('./packages/base/');
 exports.pkgProductsRoot = path.resolve ('./packages/products/');
 exports.loktharRoot     = path.resolve ('./lokthar/');
 exports.nodeModulesRoot = path.resolve ('./node_modules/');
-
