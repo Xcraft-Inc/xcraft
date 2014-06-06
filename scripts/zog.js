805c102d0a7c2acdc7ff2415dd6b09c1858af099
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-06 17:04:12 +0200

    Display the help if zog is called without argument.

--- scripts/zog.js
+++ scripts/zog.js
@@ -8,6 +8,10 @@ var zogWpkg    = require ('./zogWpkg.js');
 var zogLokthar = require ('./zogLokthar.js');
 var zogLog     = require ('./lib/zogLog.js')('zog');
 
+// Display help if zog is called without argument.
+if (process.argv.length < 3)
+  process.argv.push ('-h');
+
 program
   .version ('0.0.1')
   .option ('-v, --verbosity <level>', 'change the verbosity level [0..3] (default: 0)', zogLog.verbosity)
