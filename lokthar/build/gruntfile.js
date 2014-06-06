083aa56d0aa6af2aaa6801f6b76066860c7d80ac
Samuel Loup <samuel.loup@gmail.com>
2014-06-06 15:22:57 +0200

    Updated atom-shell runtime version

--- lokthar/build/gruntfile.js
+++ lokthar/build/gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     "download-atom-shell": {
-      version: "0.12.5",
+      version: "0.12.7",
       outputDir: "./atom-shell",
       rebuild: true
     }
