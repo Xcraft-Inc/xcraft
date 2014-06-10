505e4bf085a8299b152bf187e2023c5fe7a27788
Samuel Loup <samuel.loup@gmail.com>
2014-06-10 15:12:34 +0200

    First implementation of package editor for header fields, based on pkgWizard.js definitions

--- lokthar/lokthar-app/modules/packages/module.js
+++ lokthar/lokthar-app/modules/packages/module.js
@@ -29,6 +29,26 @@ module.controller('PackageManagerController', ['$scope', function ($scope){
 }]);
 
 module.controller('PackageEditorController', ['$scope', function ($scope){
+  var zogConfig       = require ('../../scripts/zogConfig.js');
+  var wizard          = require (zogConfig.pkgWizard);
 
+  $scope.headerFields   = wizard.header;
+  $scope.package  = [];
 
+}]);
+
+module.directive('validator', [function () {
+    return {
+        restrict: 'A',
+        scope: {
+          action: '&validator',
+        },
+        require: 'ngModel',  
+        link: function (scope, elem, attrs, control) {
+              var result = scope.action(scope.$eval(attrs.ngModel)); 
+              scope.$watch(result, function (n) {
+                control.$setValidity("unique", n);
+              });                               
+        }
+    };
 }]);
