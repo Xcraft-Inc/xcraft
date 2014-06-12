1336138c7bbd4dec35d36d9c54aa8c31f8b7caba
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-12 10:47:17 +0200

    Add a converter from inquirer definition to a package definition.

--- scripts/zogManager.js
+++ scripts/zogManager.js
@@ -2,10 +2,50 @@
 var moduleName = 'manager';
 
 var path     = require ('path');
+var util     = require ('util');
 var inquirer = require ('inquirer');
 var zogLog   = require ('./lib/zogLog.js')(moduleName);
 
 process.chdir (path.join (__dirname, '/..'));
+
+/**
+ * \brief Convert an inquirer answer to a package definition.
+ */
+var inquirerToPackage = function (inquirerPkg)
+{
+  var package = {};
+
+  inquirerPkg.forEach (function (it)
+  {
+    if (it.hasOwnProperty ('package'))
+    {
+      package.name              = it.package;
+      package.version           = it.version;
+      package.maintainer        = {};
+      package.maintainer.name   = it.maintainerName;
+      package.maintainer.email  = it.maintainerEmail;
+      package.architecture      = it.architecture;
+      package.description       = {};
+      package.description.brief = it.descriptionBrief;
+      package.description.long  = it.descriptionLong;
+      package.dependency        = {};
+    }
+    else if (it.hasOwnProperty ('hasDependency') && it.hasDependency == true)
+    {
+      if (!util.isArray (package.dependency[it.dependency]))
+        package.dependency[it.dependency] = [];
+      package.dependency[it.dependency].push (it.version);
+    }
+  });
+
+  return package;
+}
+
+var createPackageTemplate = function (inquirerPkg)
+{
+  var package = inquirerToPackage (inquirerPkg);
+  zogLog.verb ('JSON output (package):\n' + JSON.stringify (package, null, '  '));
+}
 
 var promptForDependency = function (wizard, package)
 {
@@ -16,7 +56,10 @@ var promptForDependency = function (wizard, package)
     if (answers.hasDependency)
       promptForDependency (wizard, package);
     else
-      zogLog.verb ('JSON output:\n' + JSON.stringify (package, null, '  '));
+    {
+      zogLog.verb ('JSON output (inquirer):\n' + JSON.stringify (package, null, '  '));
+      createPackageTemplate (package);
+    }
   });
 }
 
@@ -27,10 +70,10 @@ exports.create = function (packageName)
   var wizard = require ('./manager/pkgWizard.js');
 
   var package = [];
-  package.package = packageName;
 
   inquirer.prompt (wizard.header, function (answers)
   {
+    answers.package = packageName;
     package.push (answers);
     promptForDependency (wizard, package);
   });
