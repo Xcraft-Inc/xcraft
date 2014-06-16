94367213f52de4c28c96037fda571068745d41cd
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-16 17:18:18 +0200

    The method promptForDependency should be private.

--- scripts/zogManager.js
+++ scripts/zogManager.js
@@ -9,29 +9,28 @@ var pkgCreate = require ('./manager/pkgCreate.js');
 
 process.chdir (path.join (__dirname, '/..'));
 
-var promptForDependency = function (wizard, package)
-{
-  inquirer.prompt (wizard.dependency, function (answers)
-  {
-    package.push (answers);
-
-    if (answers.hasDependency)
-      promptForDependency (wizard, package);
-    else
-    {
-      zogLog.verb ('JSON output (inquirer):\n' + JSON.stringify (package, null, '  '));
-      pkgCreate.pkgTemplate (package);
-    }
-  });
-}
-
 exports.create = function (packageName)
 {
   zogLog.info ('create a new package: ' + packageName);
 
   var wizard = require ('./manager/pkgWizard.js');
-
   var package = [];
+
+  var promptForDependency = function (wizard, package)
+  {
+    inquirer.prompt (wizard.dependency, function (answers)
+    {
+      package.push (answers);
+
+      if (answers.hasDependency)
+        promptForDependency (wizard, package);
+      else
+      {
+        zogLog.verb ('JSON output (inquirer):\n' + JSON.stringify (package, null, '  '));
+        pkgCreate.pkgTemplate (package);
+      }
+    });
+  }
 
   inquirer.prompt (wizard.header, function (answers)
   {
