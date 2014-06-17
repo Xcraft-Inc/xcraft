6eefe63b6edf0e6dd341b8c79b0cdc8805c51750
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-17 12:16:01 +0200

    Add two useful paths.

--- scripts/zogConfig.js
+++ scripts/zogConfig.js
@@ -3,7 +3,9 @@ var path = require ('path');
 
 process.chdir (path.join (__dirname, '/..'));
 
+exports.toolchainRoot   = path.resolve ('./');
 exports.libRoot         = path.resolve ('./scripts/lib/');
+exports.pkgTempRoot     = path.resolve ('./tmp/');
 exports.pkgBaseRoot     = path.resolve ('./packages/base/');
 exports.pkgProductsRoot = path.resolve ('./packages/products/');
 exports.pkgCreate       = path.resolve ('./scripts/manager/pkgCreate.js');
