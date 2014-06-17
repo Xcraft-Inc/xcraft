d02190a2bf8bc1124453546f553865613c168c9a
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-17 14:52:06 +0200

    Cosmetic: move items.

--- scripts/zogConfig.js
+++ scripts/zogConfig.js
@@ -5,11 +5,12 @@ process.chdir (path.join (__dirname, '/..'));
 
 exports.toolchainRoot   = path.resolve ('./');
 exports.libRoot         = path.resolve ('./scripts/lib/');
+exports.loktharRoot     = path.resolve ('./lokthar/');
+exports.nodeModulesRoot = path.resolve ('./node_modules/');
 exports.pkgTempRoot     = path.resolve ('./tmp/');
 exports.pkgBaseRoot     = path.resolve ('./packages/base/');
 exports.pkgProductsRoot = path.resolve ('./packages/products/');
+
 exports.pkgCreate       = path.resolve ('./scripts/manager/pkgCreate.js');
 exports.pkgList         = path.resolve ('./scripts/manager/pkgList.js');
 exports.pkgWizard       = path.resolve ('./scripts/manager/pkgWizard.js');
-exports.loktharRoot     = path.resolve ('./lokthar/');
-exports.nodeModulesRoot = path.resolve ('./node_modules/');
