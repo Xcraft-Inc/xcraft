f30df019a365212c0cdc6407fbc8248d68ce651b
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-17 14:27:11 +0200

    Cosmetic.

--- scripts/manager/pkgWizard.js
+++ scripts/manager/pkgWizard.js
@@ -107,7 +107,7 @@ exports.header =
         var ms0 = answer.indexOf (os + '-i386');
         var ms1 = answer.indexOf (os + '-amd64');
         if (ms0 < 0 || ms1 < 0)
-        return;
+          return;
 
         answer.splice (ms0, 1);
         ms1 = answer.indexOf (os + '-amd64');
