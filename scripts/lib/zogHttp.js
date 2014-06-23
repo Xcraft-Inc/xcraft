d9c6d36588d00455ff5f32a0d526a757e24aa9e2
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-23 16:59:36 +0200

    Add a method to zogHttp which is able to upload a file for the zogChest server (next commits).

--- scripts/lib/zogHttp.js
+++ scripts/lib/zogHttp.js
@@ -1,7 +1,11 @@
 
+var moduleName = 'http';
+
 var fs   = require ('fs');
 var http = require ('http');
 var path = require ('path');
+
+var zogLog = require ('../lib/zogLog.js')(moduleName);
 
 exports.get = function (file_url, output_file)
 {
@@ -31,4 +35,51 @@ exports.get = function (file_url, output_file)
       file.end ();
     });
   });
+}
+
+exports.post = function (inputFile, server, port)
+{
+  var options =
+  {
+    hostname: server,
+    port    : port,
+    path    : '/upload',
+    method  : 'POST',
+    headers :
+    {
+      'Content-Type'       : 'application/octet-stream',
+      'Content-Length'     : fs.statSync (inputFile).size.toString (),
+      'Zog-Upload-Filename': path.basename (inputFile)
+    }
+  };
+
+  var request = http.request (options, function (res)
+  {
+    var body = '';
+
+    res.on ('data', function (chunk)
+    {
+      body += chunk;
+    }).on ('end', function ()
+    {
+      zogLog.verb (body);
+    }).on ('error', function (err)
+    {
+      zogLog.err ('problem with request: ' + err.message);
+    });
+  }).on ('error', function (err)
+  {
+    zogLog.err ('problem with request: ' + err.message);
+  });
+
+  var stream = fs.createReadStream (inputFile);
+
+  stream.on ('data', function (data)
+  {
+    request.write (data);
+  }).on ('end', function ()
+  {
+    zogLog.verb ('end of http POST request');
+    request.end ();
+  })
 }
