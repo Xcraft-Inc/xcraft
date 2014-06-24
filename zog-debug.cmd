d29d453cfbcdb5e8eef201375d800da97d5952cc
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-24 18:53:06 +0200

    Cosmetic: bad line feed.

--- zog-debug.cmd
+++ zog-debug.cmd
@@ -2,7 +2,6 @@
 
 if exist .\node_modules\.bin\node-debug.cmd (
   .\node_modules\.bin\node-debug scripts\zog.js %*
-
 ) else (
   echo Install node-inspector first
   echo > npm install node-inspector
