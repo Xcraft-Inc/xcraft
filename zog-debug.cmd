20791eed69474ceed7f3f40b1cf153c947ac7f51
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-24 19:24:48 +0200

    Use the port 8181 with the debugger because the chest server already uses the port 8080.

--- zog-debug.cmd
+++ zog-debug.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 if exist .\node_modules\.bin\node-debug.cmd (
-  .\node_modules\.bin\node-debug scripts\zog.js %*
+  .\node_modules\.bin\node-debug -p 8181 scripts\zog.js %*
 ) else (
   echo Install node-inspector first
   echo > npm install node-inspector
