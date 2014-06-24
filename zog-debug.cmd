6b350a766a2db6d9e1a0f3fd7f1c9c51fdee848e
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-24 18:51:20 +0200

    Add the command zog-debug. It is the same as zog but it runs the debugger in your browser.

---
+++ zog-debug.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+if exist .\node_modules\.bin\node-debug.cmd (
+  .\node_modules\.bin\node-debug scripts\zog.js %*
+
+) else (
+  echo Install node-inspector first
+  echo > npm install node-inspector
+)
