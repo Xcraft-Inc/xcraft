ed17fb9f2ebda5cabf4ccd5130aa539a922580b5
Mathieu Schroeter <schroeter@epsitec.ch>
2014-06-25 10:14:43 +0200

    Fixes echo with batch.

--- zog-debug.cmd
+++ zog-debug.cmd
@@ -4,5 +4,5 @@ if exist .\node_modules\.bin\node-debug.cmd (
   .\node_modules\.bin\node-debug -p 8181 scripts\zog.js %*
 ) else (
   echo Install node-inspector first
-  echo > npm install node-inspector
+  echo ^> npm install node-inspector
 )
