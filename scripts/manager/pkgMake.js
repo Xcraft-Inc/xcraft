1a3b13694f1177f6136b40add74887e382e56b4e
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-01 14:01:41 +0200

    Copy the templates in the package (temporary) directory. It is only stubs and it should not be copied every time.

--- scripts/manager/pkgMake.js
+++ scripts/manager/pkgMake.js
@@ -2,8 +2,25 @@
 var moduleName = 'manager';
 
 var path       = require ('path');
+var zogConfig  = require ('../zogConfig.js') ();
+var zogFs      = require ('../lib/zogFs.js');
 var zogLog     = require ('../lib/zogLog.js')(moduleName);
 var pkgControl = require ('./pkgControl.js');
+
+var copyTemplateFiles = function (packagePath, postInstDir)
+{
+  var zogPlatform = require ('../lib/zogPlatform.js');
+
+  var installerFileIn  = path.join (zogConfig.pkgTemplatesRoot, zogConfig.pkgInstaller);
+  var installerFileOut = path.join (postInstDir, zogConfig.pkgInstaller);
+
+  zogFs.cp (installerFileIn, installerFileOut);
+
+  var postinstFileIn  = path.join (zogConfig.pkgTemplatesRoot, zogConfig.pkgPostinst);
+  var postinstFileOut = path.join (packagePath, 'wpkg', zogConfig.pkgPostinst);
+
+  zogFs.cp (postinstFileIn, postinstFileOut);
+}
 
 exports.package = function (packageName, callbackDone)
 {
@@ -15,6 +32,15 @@ exports.package = function (packageName, callbackDone)
     controlFiles.forEach (function (controlFile)
     {
       var packagePath = path.resolve (path.dirname (controlFile), '..');
+
+      /* Reserved directory for the post-installer. */
+      var postInstDir = path.join (packagePath, 'usr', 'share', packageName);
+      zogFs.mkdir (postInstDir);
+
+      /* TODO: the templates should be copy only when it is necessary; like when
+       * a web-package and (or) an installer (msi-like) is used for example.
+       */
+      copyTemplateFiles (packagePath, postInstDir);
 
       /* Build the package with wpkg. */
       wpkgEngine.build (packagePath, function (error)
