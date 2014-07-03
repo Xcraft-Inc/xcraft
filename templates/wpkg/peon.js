c06eb96df7b39e43d5e58231ca83742bdda2ec32
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-07-03 18:28:54 +0200

    Add a naive implementation for the peon script in order to download a resource (untested).

--- templates/wpkg/peon.js
+++ templates/wpkg/peon.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log ('Peon stub, zog zog');
+var moduleName = 'peon';
+
+var fs      = require ('fs');
+var url     = require ('url');
+var zogHttp = require ('zogHttp');
+
+var config = require ('config.json');
+var urlObj = url.parse (config.uri);
+
+if (/http[s]+:/.test (urlObj.protocol)
+{
+  var outputFile = path.join (__dirname, 'cache', urlObj.pathname);
+  zogHttp.get (config.uri, outputFile);
+}
