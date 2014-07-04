93ca76d0fd3b90779ccb1e44c51d822294a420ba
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-04 14:20:35 +0200

    Fixes some typo errors with peon.js.

--- templates/wpkg/peon.js
+++ templates/wpkg/peon.js
@@ -6,10 +6,10 @@ var fs      = require ('fs');
 var url     = require ('url');
 var zogHttp = require ('zogHttp');
 
-var config = require ('config.json');
+var config = require ('./config.json');
 var urlObj = url.parse (config.uri);
 
-if (/http[s]+:/.test (urlObj.protocol)
+if (/http[s]+:/.test (urlObj.protocol))
 {
   var outputFile = path.join (__dirname, 'cache', urlObj.pathname);
   zogHttp.get (config.uri, outputFile);
