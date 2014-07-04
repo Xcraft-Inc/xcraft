9bdb31a06fcf4ec83caa8c47904d11104b71e09e
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-04 09:05:36 +0200

    The distribution field must have a slash at the end because we are not using components.

--- scripts/zogConfig.js
+++ scripts/zogConfig.js
@@ -73,7 +73,7 @@ module.exports = function ()
     pkgCfgFileName  : 'config.yaml',
     pkgInstaller    : 'peon.js',
     pkgPostinst     : 'postinst' + zogPlatform.getShellExt (),
-    pkgRepository   : 'toolchain',
+    pkgRepository   : 'toolchain/',
 
     /* Path helpers. */
     toolchainRoot   : path.resolve ('./'),
