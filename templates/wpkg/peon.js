137e0cce959bb75db285764da227ab08cfd9d56b
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-07 17:19:19 +0200

    Improve the peon script in order to be able to download (still needs to add the support with chest) a resource file.

--- templates/wpkg/peon.js
+++ templates/wpkg/peon.js
@@ -2,15 +2,55 @@
 
 var moduleName = 'peon';
 
-var fs      = require ('fs');
-var url     = require ('url');
-var zogHttp = require ('zogHttp');
-
-var config = require ('./config.json');
-var urlObj = url.parse (config.uri);
-
-if (/http[s]+:/.test (urlObj.protocol))
+var action = function ()
 {
-  var outputFile = path.join (__dirname, 'cache', urlObj.pathname);
-  zogHttp.get (config.uri, outputFile);
+  var fs      = require ('fs');
+  var url     = require ('url');
+  var path    = require ('path');
+  var zogHttp = require ('zogHttp');
+
+  var config = require ('./config.json');
+
+  return {
+    install: function ()
+    {
+      /* Looks if there is something to get and save the result in a cache/
+       * directory.
+       */
+      if (config.uri.length)
+      {
+        var urlObj = url.parse (config.uri);
+
+        if (/http[s]+:/.test (urlObj.protocol))
+        {
+          var outputFile = path.join (__dirname, 'cache', urlObj.pathname);
+
+          console.log ('download %s to %s', config.uri, outputFile);
+          zogHttp.get (config.uri, outputFile);
+        }
+      }
+
+      switch (config.type)
+      {
+      case 'bin':
+        break;
+
+      default:
+        break;
+      }
+    },
+
+    remove: function ()
+    {
+
+    }
+  }
+}
+
+var main = new action ();
+
+if (process.argv.length >= 3)
+{
+  console.log ('run the action: ' + process.argv[2]);
+  main[process.argv[2]] ();
 }
