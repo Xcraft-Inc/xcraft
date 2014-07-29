f374614d442ca043f9ec0e59f92896c67c68bd46
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-07-29 11:18:36 +0200

    Add a FIXME about the possibility (in the future) to embed an msi/setup in a deb. Currently only the 'not-embedded' way is wotking with this type of installer.

--- scripts/manager/pkgMake.js
+++ scripts/manager/pkgMake.js
@@ -117,6 +117,11 @@ var processCtrlFile = function (packageName, callbackDone)
         var rulesType = packageDef.data.rules.type;
         var uri       = packageDef.data.uri;
         zogPeon[dataType][rulesType] (zogUri.realUri (uri, packageName), packagePath, function (done)
+
+        /* FIXME: handle the case where we must embed an MSI. In this case
+         *        the 'exec' or 'copy' rules should not be called like for
+         *        the simple copy of files.
+         */
         {
           if (done)
             wpkgBuild (packageDef);
