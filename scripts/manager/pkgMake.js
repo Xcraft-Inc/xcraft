7db9e7eb823488c7c18394146373fec3e649210d
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-07-29 10:42:25 +0200

    Cosmetic: reduce the level of indentation by using a new function.

--- scripts/manager/pkgMake.js
+++ scripts/manager/pkgMake.js
@@ -50,111 +50,116 @@ var createConfigJson = function (packageName, postInstDir)
   fs.writeFileSync (outFile, data, 'utf8');
 };
 
+var processCtrlFile = function (packageName, callbackDone)
+{
+  var i = 0;
+  var controlFiles = pkgControl.controlFiles (packageName, true);
+
+  var wpkgEngine = require ('./wpkgEngine.js');
+
+  var nextCtrlFile = function ()
+  {
+    var controlFile = controlFiles[i];
+
+    zogLog.info ('process ' + controlFile);
+
+    var packagePath = path.resolve (path.dirname (controlFile), '..');
+
+    /* Reserved directory for the post-installer. */
+    var namespace = '';
+    var name = packageName;
+    var fullName = packageName.match (/(.*)\+(.*)/);
+    if (fullName)
+    {
+      namespace = fullName[1];
+      name      = fullName[2];
+    }
+
+    var sharePath = path.join (packagePath, 'usr', 'share', namespace, name);
+    zogFs.mkdir (sharePath);
+
+    var build = function ()
+    {
+      var wpkgBuild = function (packageDef)
+      {
+        /* Don't copy pre/post scripts with unsupported architectures. */
+        if (packageDef.architecture.indexOf ('all') === -1)
+          copyTemplateFiles (packagePath, sharePath);
+
+        createConfigJson (packageName, sharePath);
+
+        /* Build the package with wpkg. */
+        wpkgEngine.build (packagePath, function (error)
+        {
+          /* When we reach the last item, then we have done all async work. */
+          if (i == controlFiles.length - 1)
+          {
+            if (callbackDone)
+              callbackDone (true);
+          }
+          else
+          {
+            i++;
+            nextCtrlFile ();
+          }
+        });
+      };
+
+      var packageDef = pkgControl.loadPackageDef (packageName);
+
+      /* Are the resources embedded in the package (less than 1GB)? */
+      if (packageDef.data.embedded && packageDef.data.uri.length)
+      {
+        var zogPeon = require ('zogPeon');
+        var zogUri  = require ('zogUri');
+
+        var dataType  = packageDef.data.type;
+        var rulesType = packageDef.data.rules.type;
+        var uri       = packageDef.data.uri;
+        zogPeon[dataType][rulesType] (zogUri.realUri (uri, packageName), packagePath, function (done)
+        {
+          if (done)
+            wpkgBuild (packageDef);
+          else
+            zogLog.err ('can not build ' + packageName);
+        });
+      }
+      else
+        wpkgBuild (packageDef);
+    };
+
+    /* Look for premake script. */
+    try
+    {
+      var productPath = path.join (zogConfig.pkgProductsRoot, packageName);
+      var premake = require (path.join (productPath, 'premake.js')) (zogConfig, packagePath, sharePath);
+      premake.copy (function (done)
+      {
+        if (done)
+          build ();
+      });
+    }
+    catch (err)
+    {
+      if (err.code === 'MODULE_NOT_FOUND')
+      {
+        zogLog.info ('no premake script for this package');
+        build ();
+      }
+      else
+        zogLog.err (err);
+    }
+  };
+
+  if (controlFiles.length)
+    nextCtrlFile ();
+}
+
 exports.package = function (packageName, callbackDone)
 {
   try
   {
-    var i = 0;
-    var controlFiles = pkgControl.controlFiles (packageName, true);
-
-    var wpkgEngine = require ('./wpkgEngine.js');
-
-    var nextCtrlFile = function ()
-    {
-      var controlFile = controlFiles[i];
-
-      zogLog.info ('process ' + controlFile);
-
-      var packagePath = path.resolve (path.dirname (controlFile), '..');
-
-      /* Reserved directory for the post-installer. */
-      var namespace = '';
-      var name = packageName;
-      var fullName = packageName.match (/(.*)\+(.*)/);
-      if (fullName)
-      {
-        namespace = fullName[1];
-        name      = fullName[2];
-      }
-
-      var sharePath = path.join (packagePath, 'usr', 'share', namespace, name);
-      zogFs.mkdir (sharePath);
-
-      var build = function ()
-      {
-        var packageDef = pkgControl.loadPackageDef (packageName);
-
-        var wpkgBuild = function ()
-        {
-          /* Don't copy pre/post scripts with unsupported architectures. */
-          if (packageDef.architecture.indexOf ('all') === -1)
-            copyTemplateFiles (packagePath, sharePath);
-
-          createConfigJson (packageName, sharePath);
-
-          /* Build the package with wpkg. */
-          wpkgEngine.build (packagePath, function (error)
-          {
-            /* When we reach the last item, then we have done all async work. */
-            if (i == controlFiles.length - 1)
-            {
-              if (callbackDone)
-                callbackDone (true);
-            }
-            else
-            {
-              i++;
-              nextCtrlFile ();
-            }
-          });
-        };
-
-        /* Are the resources embedded in the package (less than 1GB)? */
-        if (packageDef.data.embedded && packageDef.data.uri.length)
-        {
-          var zogPeon = require ('zogPeon');
-          var zogUri  = require ('zogUri');
-
-          var dataType  = packageDef.data.type;
-          var rulesType = packageDef.data.rules.type;
-          var uri       = packageDef.data.uri;
-          zogPeon[dataType][rulesType] (zogUri.realUri (uri, packageName), packagePath, function (done)
-          {
-            if (done)
-              wpkgBuild ();
-            else
-              zogLog.err ('can not build ' + packageName);
-          });
-        }
-        else
-          wpkgBuild ();
-      };
-
-      /* Look for premake script. */
-      try
-      {
-        var productPath = path.join (zogConfig.pkgProductsRoot, packageName);
-        var premake = require (path.join (productPath, 'premake.js')) (zogConfig, packagePath, sharePath);
-        premake.copy (function (done)
-        {
-          if (done)
-            build ();
-        });
-      }
-      catch (err)
-      {
-        if (err.code === 'MODULE_NOT_FOUND')
-        {
-          zogLog.info ('no premake script for this package');
-          build ();
-        }
-        else
-          zogLog.err (err);
-      }
-    };
-
-    if (controlFiles.length)
-      nextCtrlFile ();
+    processCtrlFile (packageName, callbackDone);
   }
   catch (err)
   {
