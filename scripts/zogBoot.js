bb53b5795f43d302ab8c6c2cab6cb8a965b20103
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-31 16:46:51 +0200

    Add the zogrc path in the PATH with zogBoot.

--- scripts/zogBoot.js
+++ scripts/zogBoot.js
@@ -1,12 +1,20 @@
 'use strict';
 
+var zogConfig = require ('./zogConfig.js') ();
+
 var bootEnv = function ()
 {
   var path = require ('path');
+  var fs   = require ('fs');
 
   var list = process.env.PATH.split (path.delimiter);
   list.unshift (path.resolve ('./var/devroot/bin/'));
   list.unshift (path.resolve ('./var/devroot/usr/bin/'));
+
+  var zogrc = fs.readFileSync (zogConfig.zogRc, 'utf8');
+  if (zogrc.hasOwnProperty ('path'))
+    list.unshift (zogrc.path);
+
   process.env.PATH = list.join (path.delimiter);
 };
 
