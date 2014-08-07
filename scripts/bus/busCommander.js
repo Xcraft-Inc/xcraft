ff7f14089035c8ad12d0ccceeb4f4b5a0f4d9d2e
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-07 10:33:54 +0200

    Cosmetics: coding style and more log messages.

--- scripts/bus/busCommander.js
+++ scripts/bus/busCommander.js
@@ -2,41 +2,43 @@
 'use strict';
 
 var moduleName = 'command-bus';
+
 var zogConfig  = require ('../zogConfig.js') ();
 var zogLog     = require ('zogLog') (moduleName);
-
 var axon       = require ('axon');
-var sock       = axon.socket ('pull');
 
+var sock = axon.socket ('pull');
 var commandsRegistry = {};
+
 
 module.exports = function ()
 {
   return {
-    bus   : sock,
-    start : function (host, port, callback)
+    bus  : sock,
+    start: function (host, port, callback)
     {
-      //create domain for catching port binding errors
-      var d = require('domain').create();
+      /* Create domain in order to catch port binding errors. */
+      var domain = require ('domain').create ();
 
-      //error management
-      d.on('error', function(err){
+      domain.on ('error', function (err)
+      {
         zogLog.warn ('Bus already started on %s:%d', host, port);
       });
 
-      //try binding in domain
-      d.run(function(){
-        sock.bind (parseInt(port), host, callback);
+      /* Try binding in domain. */
+      domain.run (function ()
+      {
+        sock.bind (parseInt (port), host, callback);
         zogLog.verb ('Bus started on %s:%d', host, port);
       });
-
     },
-    stop : function ()
+
+    stop: function ()
     {
       sock.close ();
     },
 
-    registerCommandHandler : function (commandKey, handlerFunction)
+    registerCommandHandler: function (commandKey, handlerFunction)
     {
       zogLog.verb ('Command \'%s\' registered', commandKey);
       commandsRegistry[commandKey] = handlerFunction;
@@ -46,9 +48,9 @@ module.exports = function ()
 
 sock.on ('message', function (cmd, data)
 {
-  zogLog.verb ('command received: %s -> data:%s',
-               cmd,
-               JSON.stringify (data));
-  //call handler
+  zogLog.info ('begin command: %s', cmd);
+  zogLog.verb ('command received: %s -> data: %s', cmd, JSON.stringify (data));
+
+  /* call handler */
   commandsRegistry[cmd](data);
 });
