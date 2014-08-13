572d598f5dda0c85f4bdf0da48b4562eb9af0a73
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-13 15:31:01 +0200

    Prefer mingw32-make over make.

--- scripts/zogCMake.js
+++ scripts/zogCMake.js
@@ -30,7 +30,8 @@ var makeRun = function (callback)
   /* Force 1 on Windows because sometimes it fails with the depends. */
   args.unshift ('-j', zogPlatform.getOs () !== 'win' ? os.cpus ().length : '1');
 
-  var make = zogProcess.spawn ('make', args, function (done)
+  var makeBin = zogPlatform.getOs () === 'win' ? 'mingw32-make' : 'make';
+  var make = zogProcess.spawn (makeBin, args, function (done)
   {
     if (done)
       zogLog.info ('cmake is built and installed');
