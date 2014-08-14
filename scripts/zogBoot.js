384cb0698f2d91a95cee9ad47f861fa3723e36c5
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-14 11:03:47 +0200

    We drop most of entry in PATH; but we must keep c:\windows\system32 because it is the location of cmd.exe. The console is used by child_process.exec. On Unix it is not necessary because it uses the absolute path to /bin/sh.

--- scripts/zogBoot.js
+++ scripts/zogBoot.js
@@ -13,7 +13,28 @@ var bootEnv = function ()
   var path = require ('path');
   var fs   = require ('fs');
 
+  var zogPlatform = require ('zogPlatform');
+
   var list = process.env.PATH.split (path.delimiter);
+
+  /* With Windows, we must find cmd.exe or the exec() function fails.
+   * It should not be necessary on Unix because it is always related to
+   * /bin/sh which is absolute.
+   */
+  if (zogPlatform.getOs () === 'win')
+  {
+    var systemDir = path.dirname (process.env.COMSPEC).replace (/\\/g, '\\\\');
+
+    if (systemDir.length)
+    {
+      var regex = new RegExp ('^' + systemDir, 'i');
+
+      list = list.filter (function (location)
+      {
+        return regex.test (location);
+      });
+    }
+  }
 
   try
   {
