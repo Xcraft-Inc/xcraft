a2c18026004628ea90345d27324287082c9c3f9f
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-14 13:45:39 +0200

    Do not use mingw32-make because the build fails.

--- scripts/zogCMake.js
+++ scripts/zogCMake.js
@@ -30,8 +30,7 @@ var makeRun = function (callback)
   /* Force 1 on Windows because sometimes it fails with the depends. */
   args.unshift ('-j', zogPlatform.getOs () !== 'win' ? os.cpus ().length : '1');
 
-  var makeBin = zogPlatform.getOs () === 'win' ? 'mingw32-make' : 'make';
-  var make = zogProcess.spawn (makeBin, args, function (done)
+  var make = zogProcess.spawn ('make', args, function (done)
   {
     if (done)
       zogLog.info ('cmake is built and installed');
