f17c93ede8baefc1c5e97bbd1733535f2cd1e5f8
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-28 10:56:40 +0200

    Nore more dependency to jshint2.

--- .jshint.cmd
+++ .jshint.cmd
@@ -7,14 +7,8 @@ setlocal enabledelayedexpansion
 for %%F in ("%node%") do set PATH=%%~dpF;!PATH!
 
 if exist .\node_modules\grunt-contrib-jshint (
-  if exist .\node_modules\grunt-jshint2 (
-    .\node_modules\.bin\grunt --force jshint jshint2 %*
-
-  ) else (
-    echo Install node-inspector first
-    echo ^> .npm.cmd install grunt-jshint2
-  )
+    .\node_modules\.bin\grunt --force jshint %*
 ) else (
-  echo Install node-inspector first
-  echo ^> .npm.cmd install grunt-contrib-jshint grunt-jshint2
+  echo Install grunt-contrib-jshint first
+  echo ^> .npm.cmd install grunt-contrib-jshint
 )
