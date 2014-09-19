cf00b26ff24f94f8f36936df400d16c077f5a0f9
Mathieu Schroeter <schroeter@epsitec.ch>
2014-09-19 17:04:25 +0200

    Cosmetics: fixes coding style.

--- scripts/zogCMake.js
+++ scripts/zogCMake.js
@@ -16,13 +16,11 @@ var cmd = {};
 
 
 /* TODO: must be generic. */
-var makeRun = function (callback)
-{
+var makeRun = function (callback) {
   zogLog.info ('begin building of cmake');
 
   var os = require ('os');
-  var args =
-  [
+  var args = [
     'all',
     'install'
   ];
@@ -30,42 +28,35 @@ var makeRun = function (callback)
   /* Force 1 on Windows because sometimes it fails with the depends. */
   args.unshift ('-j', zogPlatform.getOs () !== 'win' ? os.cpus ().length : '1');
 
-  zogProcess.spawn ('make', args, function (done)
-  {
-    if (done)
+  zogProcess.spawn ('make', args, function (done) {
+    if (done) {
       zogLog.info ('cmake is built and installed');
+    }
 
     callback (done ? null : 'make failed');
-  }, function (line)
-  {
+  }, function (line) {
     zogLog.verb (line);
-  }, function (line)
-  {
+  }, function (line) {
     zogLog.warn (line);
   });
 };
 
 /* TODO: must be generic. */
-var bootstrapRun = function (cmakeDir, callback)
-{
+var bootstrapRun = function (cmakeDir, callback) {
   /* FIXME, TODO: use a backend (a module) for building cmake. */
   /* bootstrap --prefix=/mingw && make && make install */
 
-  var args =
-  [
+  var args = [
     'bootstrap',
     '--prefix=' + path.resolve (pkgConfig.out)
   ];
 
   process.chdir (cmakeDir);
-  zogProcess.spawn ('sh', args, function (done)
-  {
+  zogProcess.spawn ('sh', args, function (done) {
     callback (done ? null : 'bootstrap failed');
-  }, function (line)
-  {
+  }, function (line) {
     zogLog.verb (line);
-  }, function (line)
-  {
+  }, function (line) {
     zogLog.warn (line);
   });
 };
@@ -73,45 +64,39 @@ var bootstrapRun = function (cmakeDir, callback)
 /**
  * Install the cmake package.
  */
-cmd.install = function ()
-{
+cmd.install = function () {
   var archive = path.basename (pkgConfig.src);
   var inputFile  = pkgConfig.src;
   var outputFile = path.join (zogConfig.tempRoot, 'src', archive);
 
   async.auto (
   {
-    taskHttp: function (callback)
-    {
+    taskHttp: function (callback) {
       var zogHttp = require ('zogHttp');
 
-      zogHttp.get (inputFile, outputFile, function ()
-      {
+      zogHttp.get (inputFile, outputFile, function () {
         callback ();
       });
     },
 
-    taskExtract: [ 'taskHttp', function (callback)
-    {
+    taskExtract: ['taskHttp', function (callback) {
       var zogExtract = require ('zogExtract');
       var outDir = path.dirname (outputFile);
 
-      zogExtract.targz (outputFile, outDir, null, function (done)
-      {
+      zogExtract.targz (outputFile, outDir, null, function (done) {
         callback (done ? null : 'extract failed', path.join (outDir, path.basename (outputFile, '.tar.gz')));
       });
     }],
 
-    taskBootstrap: [ 'taskExtract', function (callback, results)
-    {
+    taskBootstrap: ['taskExtract', function (callback, results) {
       bootstrapRun (results.taskExtract, callback);
     }],
 
-    taskMake: [ 'taskBootstrap', makeRun ]
-  }, function (err)
-  {
-    if (err)
+    taskMake: ['taskBootstrap', makeRun]
+  }, function (err) {
+    if (err) {
       zogLog.err (err);
+    }
 
     busClient.events.send ('zogCMake.install.finished');
   });
@@ -120,8 +105,7 @@ cmd.install = function ()
 /**
  * Uninstall the cmake package.
  */
-cmd.uninstall = function ()
-{
+cmd.uninstall = function () {
   zogLog.warn ('the uninstall action is not implemented');
   busClient.events.send ('zogCMake.uninstall.finished');
 };
@@ -130,14 +114,11 @@ cmd.uninstall = function ()
  * Retrieve the list of available commands.
  * @returns {Object[]} The list of commands.
  */
-exports.busCommands = function ()
-{
+exports.busCommands = function () {
   var list = [];
 
-  Object.keys (cmd).forEach (function (action)
-  {
-    list.push (
-    {
+  Object.keys (cmd).forEach (function (action) {
+    list.push ({
       name   : action,
       handler: cmd[action]
     });
