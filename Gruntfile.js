c4221bebe9a7c98e8f0c9d9e28f550658f132a67
Mathieu Schroeter <schroeter@epsitec.ch>
2014-09-19 17:01:26 +0200

    Cosmetics: fixes coding style.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,26 +11,22 @@ var zogLog     = require ('zogLog') (moduleName);
 var pkgControl = require (zogConfig.libPkgControl);
 var pkgMake    = require (zogConfig.libPkgMake);
 
-var initNewer = function ()
-{
+var initNewer = function () {
   var list = {};
   var srcYaml = zogFs.lsdir (zogConfig.pkgProductsRoot);
 
   /* Loop for each package available in the products directory. */
-  srcYaml.forEach (function (packageName)
-  {
+  srcYaml.forEach (function (packageName) {
     var destControl = pkgControl.controlFiles (packageName, null, false);
 
     /* Loop for each control file path. */
-    destControl.forEach (function (controlFile)
-    {
+    destControl.forEach (function (controlFile) {
       list[packageName + '/' + controlFile.arch] =
       {
         src: path.join (zogConfig.pkgProductsRoot, packageName, zogConfig.pkgCfgFileName),
         dest: controlFile.control,
-        options:
-        {
-          tasks: [ 'zogMake:' + packageName + '/' + controlFile.arch ]
+        options: {
+          tasks: ['zogMake:' + packageName + '/' + controlFile.arch]
         }
       };
     });
@@ -41,69 +37,55 @@ var initNewer = function ()
 
 var listNewer = initNewer ();
 
-module.exports = function (grunt)
-{
-  grunt.initConfig (
-  {
+module.exports = function (grunt) {
+  var jsSrc = [
+    './Gruntfile.js',
+    './scripts/**/*.js',
+    './lokthar/lokthar-app/*.js',
+    './lokthar/lokthar-app/modules/**/*.js',
+    '!./lokthar/lokthar-app/js',
+    './node_modules/zog**/**/*.js',
+    './packages/products/**/*.js',
+    './tests/**/*.js',
+  ];
+
+  grunt.initConfig ({
     zogMake: {},
     newer: listNewer,
 
-    jshint:
-    {
-      options:
-      {
+    jshint: {
+      options: {
         jshintrc: '.jshintrc'
       },
-      all:
-      {
-        src:
-        [
-          './Gruntfile.js',
-          './scripts/**/*.js',
-          './lokthar/lokthar-app/*.js',
-          './lokthar/lokthar-app/modules/**/*.js',
-          './node_modules/zog**/**/*.js',
-          './packages/products/**/*.js',
-          './tests/**/*.js',
-        ]
+      all: {
+        src: jsSrc
       }
     },
 
-    jscs:
-    {
-      options:
-      {
-        config: ".jscsrc"
+    jscs: {
+      options: {
+        config: '.jscsrc'
       },
-      src:
-      [
-        './Gruntfile.js',
-        './scripts/**/*.js',
-        './lokthar/lokthar-app/*.js',
-        './lokthar/lokthar-app/modules/**/*.js',
-        './node_modules/zog**/**/*.js',
-        './packages/products/**/*.js',
-        './tests/**/*.js',
-      ]
+      src: jsSrc
     }
   });
 
-  if (fs.existsSync ('./node_modules/grunt-contrib-jshint'))
+  if (fs.existsSync ('./node_modules/grunt-contrib-jshint')) {
     grunt.loadNpmTasks ('grunt-contrib-jshint');
-  if (fs.existsSync ('./node_modules/grunt-jscs'))
+  }
+  if (fs.existsSync ('./node_modules/grunt-jscs')) {
     grunt.loadNpmTasks ('grunt-jscs');
+  }
   grunt.loadNpmTasks ('grunt-newer-explicit');
 
-  grunt.registerTask ('zogMake', 'Task to make control files on newer versions.', function (target)
-  {
+  grunt.registerTask ('zogMake', 'Task to make control files on newer versions.', function (target) {
     var done = this.async ();
     var packageName = target.replace (/\/.*/, '');
     var arch        = target.replace (/.*\//, '');
 
     zogLog.info ('make the control file for ' + packageName + ' on ' + arch);
 
-    pkgMake.package (packageName, arch, function (error)
-    {
+    pkgMake.package (packageName, arch, function (error) {
       done (error);
     });
   });
