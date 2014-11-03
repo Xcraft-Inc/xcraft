0a06149a81da230fee0fc45ad8d6406ec1fa3c83
Mathieu Schroeter <schroeter@epsitec.ch>
2014-11-03 16:09:27 +0100

    Fixes call on zog Shell.

--- zog.cmd
+++ zog.cmd
@@ -1,3 +1,7 @@
 @echo off
 
-.\.node.cmd .\scripts\zog.js %*
+call .\.node.cmd passive
+setlocal enabledelayedexpansion
+for %%F in ("%node%") do set PATH=%%~dpF;!PATH!
+
+.\node_modules\.bin\zog.cmd %*
