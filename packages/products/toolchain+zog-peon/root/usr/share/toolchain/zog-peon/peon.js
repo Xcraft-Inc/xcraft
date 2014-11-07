f31e59a61110925c5a47a0eea3dac656c7f65148
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-11-07 07:29:18 +0100

    Rename zogPeon to xPeon.

--- packages/products/toolchain+zog-peon/root/usr/share/toolchain/zog-peon/peon.js
+++ packages/products/toolchain+zog-peon/root/usr/share/toolchain/zog-peon/peon.js
@@ -1,16 +1,17 @@
 'use strict';
 
 var Action = function (currentDir) {
-  var fs      = require ('fs');
-  var path    = require ('path');
-  var zogPeon = require ('xcraft-core-peon');
+  var fs   = require ('fs');
+  var path = require ('path');
+
+  var xPeon = require ('xcraft-core-peon');
 
   var config = JSON.parse (fs.readFileSync (path.join (currentDir, './config.json')));
 
   var peonRun = function (extra) {
     console.log ('command: %s %s', extra.location, extra.args);
 
-    zogPeon[config.type][config.rules.type] (config.uri, null, currentDir, extra, function (done) {
+    xPeon[config.type][config.rules.type] (config.uri, null, currentDir, extra, function (done) {
       if (!done) {
         console.error ('can not %s %s', config.rules.type, config.type);
         process.exit (1);
