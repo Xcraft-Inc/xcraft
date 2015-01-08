3b2e3213684ac00354c5e2ec6868783a1d1d97f8
Samuel Loup <loup@epsitec.ch>
2015-01-08 08:10:40 +0100

    new layout

--- var/app/lokthar-react-app/app-template.jsx
+++ var/app/lokthar-react-app/app-template.jsx
@@ -3,20 +3,44 @@ var ipc               = require ('ipc');
 var React             = require ('react');
 var xCraftMaterials   = require ('./xcraft-materials.js');
 
-var Titlebar     = xCraftMaterials.Titlebar;
-var Lokthar      = React.createClass ({
+var Workspace     = xCraftMaterials.Workspace;
+var Titlebar      = xCraftMaterials.Titlebar;
+var Lokthar       = React.createClass ({
+  getInitialState: function () {
+    return {isMaximized: false};
+  },
 
   render: function () {
     return (
-      <div>
-        <Titlebar title="Lokthar" closeAction={this._closeLokthar} />
-      </div>
+
+      <Workspace name="app">
+        <Titlebar
+          title="Lokthar"
+          isMaximized={this.state.isMaximized}
+          closeAction={this._close}
+          minimizeAction={this._minimize}
+          maximizeAction={this._maximize} />
+        <Workspace name="main" />
+      </Workspace>
     );
   },
 
-  _closeLokthar: function () {
-    console.log ('close!');
-    ipc.send('close-app', '');
+  _close: function () {
+    ipc.send ('close-app', '');
+  },
+
+  _minimize: function () {
+    ipc.send ('minimize', '');
+  },
+
+  _maximize: function () {
+    if (!this.state.isMaximized) {
+      ipc.send ('maximize', '');
+      this.setState ({isMaximized: true});
+    } else {
+      ipc.send ('unmaximize', '');
+      this.setState ({isMaximized: false});
+    }
   }
 
 });
