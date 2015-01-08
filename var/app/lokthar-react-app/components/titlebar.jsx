ca27063474c314610d0a2a88b6928afc510d1bcf
Samuel Loup <loup@epsitec.ch>
2015-01-08 08:09:58 +0100

    updated component with better handling and look

--- var/app/lokthar-react-app/components/titlebar.jsx
+++ var/app/lokthar-react-app/components/titlebar.jsx
@@ -1,37 +1,51 @@
 var React      = require ('react');
 var mui        = require ('material-ui');
 
-
+var Classable    = mui.Mixins.Classable;
+var AppBar       = mui.AppBar;
 var FlatButton   = mui.FlatButton;
+var IButton      = mui.IconButton;
 var Toolbar      = mui.Toolbar;
 var ToolbarGroup = mui.ToolbarGroup;
 var Titlebar     = React.createClass ({
 
+  mixins: [Classable],
+
   propTypes: {
     title: React.PropTypes.string,
+    isMaximized: React.PropTypes.bool,
+    minimizeAction: React.PropTypes.func,
+    maximizeAction: React.PropTypes.func,
     closeAction: React.PropTypes.func
   },
 
   render: function () {
+    var maximizeIcon = this.props.isMaximized ? 'navigation-fullscreen-exit' : 'navigation-fullscreen';
+    var maximizeTip = this.props.isMaximized ? 'Exit fullscreen' : 'Fullscreen';
     return (
-      <Toolbar>
-        <ToolbarGroup float="left">
-          <h1>{this.props.title}</h1>
-        </ToolbarGroup>
-        <ToolbarGroup float="right">
-          <FlatButton
-            key={0}
-            label="X"
-            primary={true}
-            onTouchTap={this._handleCloseTouchTap} />
-        </ToolbarGroup>
-      </Toolbar>
+      <AppBar title={this.props.title}>
+        <IButton className="xc-titlebar-actions" icon="navigation-close" tooltip="Exit" onTouchTap={this._handleCloseTouchTap} />
+        <IButton className="xc-titlebar-actions" icon={maximizeIcon} tooltip={maximizeTip} onTouchTap={this._handleMaximizeTouchTap} />
+        <IButton className="xc-titlebar-actions" icon="navigation-expand-more" tooltip="Minimize" onTouchTap={this._handleMiminizeTouchTap} />
+      </AppBar>
     );
   },
 
   _handleCloseTouchTap: function() {
     if (this.props.closeAction) {
       this.props.closeAction ();
+    }
+  },
+
+  _handleMiminizeTouchTap: function() {
+    if (this.props.minimizeAction) {
+      this.props.minimizeAction ();
+    }
+  },
+
+  _handleMaximizeTouchTap: function() {
+    if (this.props.maximizeAction) {
+      this.props.maximizeAction ();
     }
   }
 
