7e6f008ca6abf63dfc1614f6629035f2dde7c888
Samuel Loup <loup@epsitec.ch>
2015-01-08 14:58:31 +0100

    the lib is now loading reactions for components

--- var/app/lokthar-react-app/xcraft-materials.js
+++ var/app/lokthar-react-app/xcraft-materials.js
@@ -1,10 +1,14 @@
-var injectTapEventPlugin = require('./node_modules/material-ui/node_modules/react-tap-event-plugin');
-
+var injectTapEventPlugin = require ('./node_modules/material-ui/node_modules/react-tap-event-plugin');
+var xcraftReaction       = require ('./actions/xcraftReaction.js');
 // Needed for onTouchTap
 // Can go away when react 1.0 release
 // Check this repo:
 // https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
+injectTapEventPlugin ();
+
+
+// Init xcraftBus2app
+xcraftReaction ();
 
 module.exports = {
   Workspace: require ('./components/workspace.jsx'),
