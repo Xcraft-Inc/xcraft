0dfcb652c7c4782fb72ffe6d9b64597019de57b4
Samuel Loup <samuel.loup@gmail.com>
2015-01-09 15:11:15 +0100

    apply css class in some cases

--- var/app/lokthar-react-app/components/workspace.jsx
+++ var/app/lokthar-react-app/components/workspace.jsx
@@ -1,19 +1,27 @@
 var React      = require ('react');
 var mui        = require ('material-ui');
-
+var Classable  = mui.Mixins.Classable;
 
 var AppCanvas    = mui.AppCanvas;
 
 var Workspace    = React.createClass ({
 
+  mixins: [Classable],
+
   propTypes: {
-    name: React.PropTypes.string
+    name: React.PropTypes.string,
+    layout: React.PropTypes.number
   },
 
   render: function () {
+    var classes = this.getClasses({
+      'mui-app-content-canvas': this.props.layout === 0
+    });
     return (
-      <AppCanvas predefinedLayout={1}>
-        {this.props.children}
+      <AppCanvas predefinedLayout={this.props.layout}>
+        <div className={classes}>
+          {this.props.children}
+        </div>
       </AppCanvas>
     );
   }
