1357e35e4ab2e11cd30868870ae5475aad62ab54
Samuel Loup <samuel.loup@gmail.com>
2015-01-12 16:13:06 +0100

    package def and postmake script

---
+++ packages/toolchain+xcraft-atom/postmake.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function (packagePath, sharePath) {
+  var path = require ('path');
+  var packageName = path.basename (__dirname);
+
+  var xLog = require ('xcraft-core-log') (packageName);
+  var xFs  = require ('xcraft-core-fs');
+
+  var xcraftInstall = function (callback) {
+    var xPlatform = require ('xcraft-core-platform');
+    var xProcess  = require ('xcraft-core-process');
+
+    xLog.info ('postmake for xcraft-atom');
+
+    var xcraft = 'xcraft' + xPlatform.getCmdExt ();
+    var args = [
+      '--modprefix', sharePath,
+      'install', sharePath
+    ];
+
+    xLog.verb (xcraft);
+
+    xProcess.spawn (xcraft, args, function (err) {
+      callback (err);
+    }, function (line) {
+      xLog.verb (line);
+    }, function (line) {
+      xLog.err (line);
+    });
+  };
+
+  return {
+    copy: function (callback) {
+      xcraftInstall (function (err) {
+        var nodeModules = path.join (sharePath, 'node_modules');
+
+        xFs.rmdir (nodeModules);
+        callback (err);
+      });
+    }
+  };
+};
