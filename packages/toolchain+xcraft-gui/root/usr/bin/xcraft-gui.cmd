b62ca37f2ec5642098b6215a9ad089e5822ed4a3
Samuel Loup <loup@epsitec.ch>
2015-01-14 16:22:14 +0100

    binary launchers removed

--- packages/toolchain+xcraft-gui/root/usr/bin/xcraft-gui.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\share\toolchain\xcraft-gui\bin\xcraft-gui" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\share\toolchain\xcraft-gui\bin\xcraft-gui" %*
-)
