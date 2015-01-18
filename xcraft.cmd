69570e4661b27d12b5471e9798595cc1516c2d1a
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-01-18 21:51:53 +0100

    Move xcraft in the main lib directory, it looks more appropriate.

--- xcraft.cmd
+++ xcraft.cmd
@@ -4,4 +4,4 @@ call .\.node.cmd passive
 setlocal enabledelayedexpansion
 for %%F in ("%node%") do set PATH=%%~dpF;!PATH!
 
-node .\usr\lib\xcraft\bin\xcraft.js %*
+node .\lib\xcraft\bin\xcraft.js %*
