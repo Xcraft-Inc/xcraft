75a10ef20e935923e47264b78bc7914d9fe2eccc
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-01-19 18:43:17 +0100

    Fixes the list of js files for jscs and jshint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,10 @@ var fs = require ('fs');
 module.exports = function (grunt) {
   var jsSrc = [
     './Gruntfile.js',
-    './scripts/*.js',
     './packages/**/*.js',
     './tests/**/*.js',
-    './lib/xcraft-core-*/*.js',
-    './lib/xcraft-contrib-chest/**/*.js',
-    './lib/xcraft-contrib-cmake/**/*.js',
-    './lib/xcraft-contrib-lokthar/lokthar-app/*.js',
-    './lib/xcraft-contrib-lokthar/lokthar-app/modules/**/*.js',
-    './lib/xcraft-contrib-pacman/**/*.js',
-    './lib/xcraft-contrib-wpkg/**/*.js',
-    './lib/xcraft-core-*/**/*.js',
-    './lib/xcraft-zog/**/*.js'
+    './lib/xcraft*/**/*.js',
+    './usr/src/xcraft*/**/*.js'
   ];
 
   grunt.initConfig ({
