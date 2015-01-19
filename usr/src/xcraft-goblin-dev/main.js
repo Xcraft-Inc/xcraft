fe41dfd5417b6083b871f7758061982347597992
Samuel Loup <samuel.loup@gmail.com>
2015-01-19 15:45:55 +0100

    add ipc handler for bridging bus cmd/evts

--- usr/src/xcraft-goblin-dev/main.js
+++ usr/src/xcraft-goblin-dev/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var app           = require ('app');
-var path          = require ('path');
 var ipc           = require ('ipc');
 var BrowserWindow = require ('browser-window');
 var busClient     = require ('xcraft-core-busclient');
@@ -21,9 +20,41 @@ ipc.on ('start-app', function () {
 });
 
 app.on ('ready', function () {
-  desktop = new BrowserWindow({width: 960, height: 600, frame: true});
-  desktop.loadUrl('file://' + __dirname + '/index.html');
-  desktop.on('closed', function () {
-    desktop = null;
+  var loadDesktop = function () {
+    console.log ('opening goblin desktop');
+    desktop = new BrowserWindow({width: 960, height: 600, frame: true});
+    desktop.loadUrl('file://' + __dirname + '/index.html');
+    desktop.on('closed', function () {
+      busClient.events.subscribe ('disconnected', function (msg) { /* jshint ignore:line */
+        busClient.stop (function () {
+          desktop = null;
+        });
+      });
+      busClient.command.send ('shutdown');
+    });
+  };
+
+  busClient.connect (null, function (err) {
+    if (err) {
+      console.error (err);
+      process.exit(1);
+    }
+
+    ipc.on ('subscribe-event', function (event, topic) {
+      busClient.events.subscribe (topic, function (msg) {
+        var action = topic.replace (/(.*)(\.[a-z])(.*)/, '$1' + topic.replace (/.*\.([a-z]).*/, '$1').toUpperCase () + '$3');
+        var receivedEvent = {
+          name: action,
+          msg: msg
+        };
+        event.sender.send ('trigger-event', receivedEvent);
+      });
+    });
+
+    ipc.on ('send-cmd', function (event, command) {
+      busClient.command.send (command);
+    });
+
+    loadDesktop ();
   });
 });
