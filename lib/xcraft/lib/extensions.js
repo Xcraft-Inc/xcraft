Xcraft-Inc/xcraft

commit 786fc983f0cf5f62993f1615234a7c830a1a6b68

fix some publishing and installing problems with non package directories

lib/xcraft/lib/extensions.js
@@ -110,6 +110,12 @@ var publish = function (packageToPublish, isDir, hostname, port, callback) {
     ];
 
     var packagePath = isDir ? path.resolve ('./lib/', packageToPublish) : packageToPublish;
+
+    if (!fs.existsSync (path.join (packagePath, 'package.json'))) {
+      console.log ('[' + moduleName + '] Info: not a valid npm package, skipping');
+      callback ();
+      return;
+    }
     args.push (packagePath);
 
     console.log ('[' + moduleName + '] Info: ' + npm + ' ' + argsToString (args));
@@ -458,6 +464,11 @@ cmd.cache = function (args, callback) {
  */
 cmd.install = function (modules, callback) {
   var packages    = modules.length ? modules : fs.readdirSync (path.resolve ('./lib/'));
+  packages = packages.filter (function (pkg) {
+    var packagePath = path.resolve ('./lib/', pkg);
+    return fs.existsSync (path.join (packagePath, 'package.json'));
+  });
+
   var unpmService = startUNPMService ();
 
   install (packages, true, unpmService.config.host.hostname, unpmService.config.host.port, function () {

commit 9e2a9fce07a7a21dbc2b385052fffb7acce22c46

Fixes regression introduced by 786fc983f0cf5f62993f1615234a7c830a1a6b68.

lib/xcraft/lib/extensions.js
@@ -111,8 +111,9 @@ var publish = function (packageToPublish, isDir, hostname, port, callback) {
 
     var packagePath = isDir ? path.resolve ('./lib/', packageToPublish) : packageToPublish;
 
-    if (!fs.existsSync (path.join (packagePath, 'package.json'))) {
-      console.log ('[' + moduleName + '] Info: not a valid npm package, skipping');
+    if (path.extname (packagePath) !== '.tgz' &&
+        !fs.existsSync (path.join (packagePath, 'package.json'))) {
+      console.log ('[' + moduleName + '] Info: ' + packagePath + ' is not a valid npm package, skipping');
       callback ();
       return;
     }