d75cca12dc1b022826d40e5b293d4b34b452407f
Samuel Loup <loup@epsitec.ch>
2015-02-03 06:52:15 +0100

    Better goblin starter pack

---
+++ lib/generator-xcraft/goblin/templates/home.jsx
@@ -0,0 +1,19 @@
+var React        = require ('react');
+
+var AppHome     = React.createClass ({
+
+  mixins: [],
+
+  propTypes: {
+
+  },
+
+  render: function () {
+    return (
+      <p>Gadget Ready!</p>
+    );
+  }
+
+});
+
+module.exports = AppHome;
