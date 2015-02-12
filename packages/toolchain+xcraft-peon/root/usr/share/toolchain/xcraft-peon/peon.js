45be73872eea8f8de25b149975d9ff92f626a5f4
Mathieu Schroeter <schroeter@epsitec.ch>
2015-02-12 15:59:12 +0100

    Pass the temporary binary directory to the peon.
    On the way, this commit splits the root and the share pass in two
    arguments. Note that this new CMake argument is passed only for
    the makeall hook.

--- packages/toolchain+xcraft-peon/root/usr/share/toolchain/xcraft-peon/peon.js
+++ packages/toolchain+xcraft-peon/root/usr/share/toolchain/xcraft-peon/peon.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var Action = function (currentDir) {
+var path = require ('path');
+
+var Action = function (root, currentDir, binaryDir) {
   var fs   = require ('fs');
-  var path = require ('path');
 
   var xPeon = require ('xcraft-core-peon');
 
@@ -44,8 +45,12 @@ var Action = function (currentDir) {
 };
 
 if (process.argv.length >= 4) {
-  var main = new Action (process.argv[2]);
+  var root = process.argv[2];
+  var share = path.join (root, process.argv[3]);
+  var action = process.argv[4];
+  var prefix = process.argv[5];
+  var main = new Action (root, share, prefix);
 
-  console.log ('run the action: ' + process.argv[3]);
-  main[process.argv[3]] ();
+  console.log ('run the action: ' + action);
+  main[action] ();
 }
