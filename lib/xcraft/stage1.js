Xcraft-Inc/xcraft

commit ddf430278eff4a8132819223a029d3e79a611321

Use the main root commands (xcraft and zog).

lib/xcraft/stage1.js
@@ -44,11 +44,10 @@ var installStrongDeps = function (callback) {
   }
 };
 
-/* TODO: use the main root xcraft command. */
 var execCmd = function (bin, verb, args, callback) {
   try {
-    var node = 'node';
-    var finalArgs = [bin, verb];
+    var node = bin;
+    var finalArgs = [verb];
 
     if (args.length > 0) {
       finalArgs = finalArgs.concat (args);
@@ -84,12 +83,18 @@ var execCmd = function (bin, verb, args, callback) {
   }
 };
 
+var getCmdExt = function () {
+  return /^win/.test (process.platform) ? '.cmd' : '';
+};
+
 var execXcraft = function (verb, args, callback) {
-  execCmd (path.join (__dirname, './bin/xcraft.js'), verb, args, callback);
+  var xcraft = 'xcraft' + getCmdExt ();
+  execCmd (path.resolve (xcraft), verb, args, callback);
 };
 
 var execZog = function (verb, args, callback) {
-  execCmd (path.resolve ('node_modules/xcraft-zog/bin/zog'), verb, args, callback);
+  var zog = 'zog' + getCmdExt ();
+  execCmd (path.resolve (zog), verb, args, callback);
 };
 
 console.log ('[' + moduleName + '] Info: install strong dependencies');

commit 03723d3ea5e7bd99b7900cfa98f182a182f93146

Fix the call of xcraft with stage1.
It was a regression with ddf430278eff4a8132819223a029d3e79a611321
when the etc/xcraft/config.json file doesn't exist. The main xcraft
command depends of this config file. Then we must call the js script
directly.

lib/xcraft/stage1.js
@@ -45,9 +45,20 @@ var installStrongDeps = function (callback) {
 };
 
 var execCmd = function (bin, verb, args, callback) {
+  var util = require ('util');
+
   try {
-    var node = bin;
-    var finalArgs = [verb];
+    var node = null;
+    var finalArgs = [];
+
+    if (util.isArray (bin)) {
+      node = bin[0];
+      finalArgs.push (bin[1]);
+    } else {
+      node = bin;
+    }
+
+    finalArgs.push (verb);
 
     if (args.length > 0) {
       finalArgs = finalArgs.concat (args);
@@ -88,8 +99,7 @@ var getCmdExt = function () {
 };
 
 var execXcraft = function (verb, args, callback) {
-  var xcraft = 'xcraft' + getCmdExt ();
-  execCmd (path.resolve (xcraft), verb, args, callback);
+  execCmd (['node', path.join (__dirname, './bin/xcraft.js')], verb, args, callback);
 };
 
 var execZog = function (verb, args, callback) {