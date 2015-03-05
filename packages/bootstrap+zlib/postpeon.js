99e592a55e3c66f4be137bc5e943603c27831d43
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-03-05 14:55:22 +0100

    Add a bootstrap+zlib package for unices.

---
+++ packages/bootstrap+zlib/postpeon.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (packagePath, sharePath) {
+  var path = require ('path');
+  var xFs  = require ('xcraft-core-fs');
+
+  return {
+    copy: function (callback) {
+      xFs.mv (path.join (sharePath, 'cache/data/zlib-1.2.8/ChangeLog'),
+              path.join (sharePath, 'cache/data/zlib-1.2.8/ChangeLog.orig'));
+      callback ();
+    }
+  };
+};
