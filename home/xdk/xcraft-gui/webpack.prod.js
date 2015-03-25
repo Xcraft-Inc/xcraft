c3bbbef999962757e9394ff99b377756ae5d0f3d
Samuel Loup <loup@epsitec.ch>
2015-03-25 13:21:00 +0100

    Fix various gobelin issues in xdk

--- home/xdk/xcraft-gui/webpack.prod.js
+++ home/xdk/xcraft-gui/webpack.prod.js
@@ -1,7 +1,14 @@
 /* Webpack config */
+var webpack = require ('webpack');
+
+var definePlugin = new webpack.DefinePlugin({
+	__WEBPACK__: true
+});
+
 module.exports = {
 	target: 'atom',
 	debug: false,
+	context: __dirname,
 	devtool: 'source-map',
 	entry: {
 		main: './app.jsx'
@@ -15,28 +22,16 @@ module.exports = {
 		modulesDirectories: ['bower_components', 'node_modules'],
 	},
 	module: {
-		loaders: [{
-			test: /\.css$/,
-			loader: 'style-loader!css-loader'
-		}, {
-			test: /\.less$/,
-			loader: 'style-loader!css-loader!less-loader'
-		}, {
-			test: /\.jsx$/,
-			loader: 'jsx-loader?harmony&insertPragma=React.DOM'
-		}, {
-			test: /\.png$/,
-			loader: 'url-loader?limit=100000&mimetype=image/png'
-		}, {
-			test: /\.gif$/,
-			loader: 'url-loader?limit=100000&mimetype=image/gif'
-		}, {
-			test: /\.jpg$/,
-			loader: 'url-loader?limit=100000&mimetype=image/jpg'
-		}, {
-			test: /\.(ttf|eot|svg|woff)/,
-			loader: 'file-loader'
-		}]
+		loaders: [
+		{test: /\.css$/, loader: 'style-loader!css-loader'},
+		{test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+		{test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
+		{test: /\.jsx$/, loader: 'jsx-loader?harmony&insertPragma=React.DOM'},
+		{test: /\.png$/, loader: 'url-loader?limit=100000&mimetype=image/png'},
+		{test: /\.gif$/, loader: 'url-loader?limit=100000&mimetype=image/gif'},
+		{test: /\.jpg$/, loader: 'url-loader?limit=100000&mimetype=image/jpg'},
+		{test: /\.(ttf|eot|svg|woff)/, loader: 'file-loader'}
+		]
 	},
-	plugins: []
+	plugins: [definePlugin]
 };
