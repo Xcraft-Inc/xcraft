b085afd8d1c0b1d2d174edc9ac616dbf8f7492ad
Samuel Loup <loup@epsitec.ch>
2015-03-26 21:39:24 +0100

    Change window management method

--- home/xdk/xcraft-goblin/main.js
+++ home/xdk/xcraft-goblin/main.js
@@ -6,24 +6,21 @@ var BrowserWindow = require ('browser-window');
 var busClient     = require ('xcraft-core-busclient');
 var xUtils        = require ('xcraft-core-utils');
 var desktop    = null;
-var appWindows = {};
+var appWindows = [];
 var windex = 0; // WindowIndex
 
 ipc.on ('start-app', function () {
   appWindows[windex] = new BrowserWindow({width: 960, height: 600, frame: false});
-  appWindows[windex].loadUrl('file://' + __dirname + '/../xcraft-gui/index.html');
-  appWindows[windex].on ('closed', function () {
-    appWindows[windex] = null;
-  });
+  appWindows[windex].loadUrl ('file://' + __dirname + '/../xcraft-gui/index.html');
+  appWindows[windex].toggleDevTools ();
   appWindows[windex].windex = windex;
   windex++;
 });
 
 
 ipc.on ('exit', function () {
-  Object.keys (appWindows).forEach ( function (window) {
+  appWindows.forEach ( function (window) {
     window.close ();
-    window = null;
   });
 });
 
@@ -31,9 +28,9 @@ app.on ('ready', function () {
   var loadDesktop = function () {
     console.log ('opening goblin desktop');
     desktop = new BrowserWindow({width: 960, height: 600, kiosk: false, fullscreen: true});
-    desktop.toggleDevTools();
-    desktop.loadUrl('file://' + __dirname + '/index.html');
-    desktop.on('closed', function () {
+    desktop.toggleDevTools ();
+    desktop.loadUrl ('file://' + __dirname + '/index.html');
+    desktop.on ('closed', function () {
       desktop = null;
       busClient.stop (function () {
         app.quit ();
@@ -44,24 +41,38 @@ app.on ('ready', function () {
   busClient.connect (null, function (err) {
     if (err) {
       console.error (err);
-      process.exit(1);
+      process.exit (1);
     }
     console.log ('Connected to zog daemon!');
-    ipc.on ('subscribe-event', function (event, topic) {
-      busClient.subscriptions.on ('message', function (topic, msg) {
-        var action;
-        if (msg) {
-          action = xUtils.topic2Action (topic);
-          var receivedEvent = {
-            name: action,
-            msg: msg
-          };
-          event.sender.send ('trigger-event', receivedEvent);
+
+    busClient.subscriptions.on ('message', function (topic, msg) {
+      var action;
+      if (msg) {
+        topic = topic.replace (/[^:]*::/, '');
+        action = xUtils.topic2Action (topic);
+        var receivedEvent = {
+          name: action,
+          msg: msg
+        };
+        if (topic !== 'heartbeat') {
+          console.log ('GOBLIN EVENT COMMING:' + JSON.stringify (receivedEvent));
+          appWindows.forEach ( function (window) {
+            if (window !== null) {
+              if (window.webContents !== null) {
+                console.log ('Event sent to window: ' + window.windex);
+                window.webContents.send ('trigger-event', receivedEvent);
+              } else {
+                console.log ('Closed window removed');
+                appWindows.splice (window.windex, 1);
+              }
+            }
+          });
         }
-      });
+      }
     });
-    
+
     ipc.on ('send-cmd', function (event, command) {
+      console.log ('GOBLIN SEND COMMAND:' + JSON.stringify (command));
       busClient.command.send (command);
     });
 
